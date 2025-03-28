6bc604a6110f3fb9bdb001503c63057fafd5f285
Rob Mosher <Rob.Mosher@thycotic.com>
2025-03-28 14:29:20 -0400

    DAP for C#

--- init.lua
+++ init.lua
@@ -337,6 +337,105 @@ require('lazy').setup({
         { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        -- DAP section RCM
+        {
+          '<leader>d',
+          group = 'Debugger',
+          nowait = true,
+          remap = false,
+        },
+        {
+          '<leader>dt',
+          function()
+            require('dap').toggle_breakpoint()
+          end,
+          desc = 'Toggle Breakpoint',
+          nowait = true,
+          remap = false,
+        },
+        {
+          '<leader>dc',
+          function()
+            require('dap').continue()
+          end,
+          desc = 'Continue',
+          nowait = true,
+          remap = false,
+        },
+        {
+          '<leader>di',
+          function()
+            require('dap').step_into()
+          end,
+          desc = 'Step Into',
+          nowait = true,
+          remap = false,
+        },
+        {
+          '<leader>do',
+          function()
+            require('dap').step_over()
+          end,
+          desc = 'Step Over',
+          nowait = true,
+          remap = false,
+        },
+        {
+          '<leader>du',
+          function()
+            require('dap').step_out()
+          end,
+          desc = 'Step Out',
+          nowait = true,
+          remap = false,
+        },
+        {
+          '<leader>dr',
+          function()
+            require('dap').repl.open()
+          end,
+          desc = 'Open REPL',
+          nowait = true,
+          remap = false,
+        },
+        {
+          '<leader>dl',
+          function()
+            require('dap').run_last()
+          end,
+          desc = 'Run Last',
+          nowait = true,
+          remap = false,
+        },
+        {
+          '<leader>dq',
+          function()
+            require('dap').terminate()
+            require('dapui').close()
+            require('nvim-dap-virtual-text').toggle()
+          end,
+          desc = 'Terminate',
+          nowait = true,
+          remap = false,
+        },
+        {
+          '<leader>db',
+          function()
+            require('dap').list_breakpoints()
+          end,
+          desc = 'List Breakpoints',
+          nowait = true,
+          remap = false,
+        },
+        {
+          '<leader>de',
+          function()
+            require('dap').set_exception_breakpoints { 'all' }
+          end,
+          desc = 'Set Exception Breakpoints',
+          nowait = true,
+          remap = false,
+        },
       },
     },
   },
@@ -1005,6 +1104,79 @@ require('lazy').setup({
     --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  },
+  {
+    'mfussenegger/nvim-dap',
+    event = 'VeryLazy', -- what does this mean? RCM
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'nvim-neotest/nvim-nio',
+      'jay-babu/mason-nvim-dap.nvim',
+      'theHamsta/nvim-dap-virtual-text',
+    },
+    config = function()
+      local mason_dap = require 'mason-nvim-dap'
+      local dap = require 'dap'
+      local ui = require 'dapui'
+      local dap_virtual_text = require 'nvim-dap-virtual-text'
+
+      dap_virtual_text.setup()
+
+      mason_dap.setup {
+        ensure_installed = { 'netcoredbg' },
+        automatic_installation = true,
+        handlers = {
+          function(config)
+            require('mason-nvim-dap').default_setup(config)
+          end,
+        },
+      }
+
+      -- RCM maybe not needed with mason-dap
+      -- mason_dap example doesn't have this. But errored on it not being there
+      dap.adapters.coreclr = {
+        type = 'executable',
+        command = '/home/rmosher/bin/netcoredbg',
+        args = { '--interpreter=vscode' },
+      }
+      dap.configurations = {
+        cs = { -- untested
+          {
+            type = 'coreclr',
+            name = 'launch - netcoredbg',
+            request = 'launch',
+            program = function()
+              return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file') -- Are these placeholders? RCM
+            end,
+          },
+          {
+            type = 'coreclr',
+            name = 'attach - netcoredbg',
+            request = 'attach',
+            processId = require('dap.utils').pick_process,
+          },
+        },
+      }
+
+      -- Dap UI
+
+      ui.setup()
+
+      vim.fn.sign_define('DapBreakpoint', { text = '🐞' })
+
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+    end,
   },
   {
     'nvim-neotest/neotest',
